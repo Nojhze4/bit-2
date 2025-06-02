7d5536b2e884c3302551ddb5691e2e50b783b914
Jhon Ramirez <jhramirez9716@gmail.com>
2025-06-02 14:26:26 -0500

    cambios css

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-'use strict';
+"use strict";
 const d = document;
+const $root = d.getElementById("root");
 
-const $root = d.getElementById('root');
+let cards = '<div class="d-flex flex-wrap">';
 
-let cards = `
-<div class="d-flex flex-wrap">
-`;
-
-fetch('file.json')
-  .then((res) => res.json())
+fetch("file.json")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Error en la respuesta de la red");
+    }
+    return res.json();
+  })
   .then((info) => {
-    for (let i = 0; i < info.length; i++) {
+    info.forEach((student) => {
       cards += `
-<div class="card">
-  <img class="card-img-top" src="https://github.com/${info[i].usernameGithub}.png" alt="Imagen de perfil de ${info[i].student}">
-  <div class="card-body">
-  <h5 class="card-title">${info[i].student}</h5>
-  </div>
-  <div class="card-body">
-  <a href="https://github.com/${info[i].usernameGithub}" target="_blank" rel="noopener noreferrer" class="card-link">GitHub</a>
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://github.com/${student.usernameGithub}.png" alt="Imagen de perfil de ${student.student}" onerror="this.onerror=null;this.src='not-found.png';">
+      <h5 class="card-title">${student.student}</h5>
+    </div>
+    <div class="flip-card-back">
+      <h5>${student.student}</h5>
+      <a href="https://github.com/${student.usernameGithub}" target="_blank" rel="noopener noreferrer" class="card-link">Ver GitHub</a>
+    </div>
   </div>
 </div>
-      `;
-    }
-    cards += '</div>';
+`;
+    });
+
+    cards += "</div>";
     $root.innerHTML = cards;
   })
   .catch((err) => {
-    console.log('error:', err);
+    alert("Error: " + err);
   });
